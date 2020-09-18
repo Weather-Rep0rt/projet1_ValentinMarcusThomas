980f9908f405a183845502670eb17abd5b62400b
adalor <55105374+adadlor@users.noreply.github.com>
2020-09-19 00:19:21 +0200

    Add files via upload

---
+++ prototype.py
@@ -0,0 +1,66 @@
+# Valentin/Marcus/Thomas
+#récupération des données par le biais de fichier JSON
+import requests
+
+
+class music:
+    def __init__(self, artiste, musique):
+        '''
+        fonction pour construire l'objet appartenant a la classe music
+
+        :param artiste : str
+        :param musique : str
+        :param self : obj
+
+        '''
+        self.artiste = artidte
+        self.musique = musique
+
+    def info_artiste(self, artiste):
+        '''
+        fonction qui a pour but d'aller chercher dans l'api musicbrainz les info
+        pricipale sur l'artiste et de les renvoyer
+
+        :param artiste : str
+        :param self : obj
+        '''
+
+    def musique_artiste(self, artiste):
+        '''
+        fonction qui a pour but d'aller chercher dans l'api musicbrainz les
+        musique créer/écrite/participer l'artite et de les renvoyer
+
+        :param artiste : str
+        :param self : obj
+        '''
+
+    def collab(self, artiste):
+        '''
+        fonction qui a pour but d'aller chercher dans l'api musicbrainz toute les
+        collaboration que l'artiste a fais avec d'autres artistes et de les
+        renvoyer
+
+        :param artiste : str
+        :param self : obj
+        '''
+
+    def artiste_musique(self, musique):
+        '''
+        fonction qui a pour but d'aller chercher dans l'api musicbrainz le ou
+        les artistes qui ont creer cette musique et de les renvoyer
+
+        :param musique : str
+        :param self : obj
+        '''
+
+    def info_musique(self, musique):
+        '''
+        fonction qui a pour but d'aller chercher dans l'api musicbrainz les info
+        principale de la musique et de les renvoyer
+
+        :param musique : str
+        :param self : obj
+        '''
+
+
+
